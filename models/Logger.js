d1f35201c130930b9aa13e460ad8a640059f068b
IlarionHalushka <hilarion.galushka@gmail.com>
2019-10-05 21:45:53 +0300

    feat: add logger mongoose model

---
+++ models/Logger.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const LoggerActionsSchema = new Schema(
+	{
+		data: {
+			type: Object,
+		},
+		action: {
+			type: String,
+		},
+		collectionName: {
+			type: String,
+			lowercase: true,
+		},
+		conditions: {
+			type: Object,
+		},
+		options: {
+			type: Object,
+		},
+		updatedBy: {
+			type: Schema.Types.ObjectId,
+		},
+		env: {
+			type: String,
+			default: 'local',
+		},
+	},
+	{ timestamps: true },
+);
+
+module.exports = mongoose.model('logs', LoggerActionsSchema);
